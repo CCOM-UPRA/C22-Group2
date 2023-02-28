97bdeb97d7818e7b85001d3481b6d0b9790a6b73
marrero171 <50031007+marrero171@users.noreply.github.com>
2023-02-27 22:00:10 -0400

    fix backend products controller

--- Phase-1/backend_model/productsModel.py
+++ Phase-1/backend_model/productsModel.py
@@ -7,23 +7,11 @@ productsPath = './JSONfiles/products.json'
 # Done in array instead of dictionaries to portray the differences between dictionaries and arrays
 # Database tuples are normally received in an array
 
-   
 def getProductsModel():
   # Load JSON data into a dictionary
   with open(productsPath) as f:
     data = json.load(f)
-
-    # Access keys and sub-keys and put them into a Python list
-    productList = []
-    for key in data.keys():
-     item = [key]
-    for sub_key in data[key].keys():
-        item.append(data[key][sub_key])
-    productList.append(item)
-
-
-    return productList
-
+    return data
 
 # Find the specific product given the ID, found in element 0 of the sub-arrays
 def getsingleproductmodel(prodID):
