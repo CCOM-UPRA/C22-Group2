ac08503954d53a4ecdfb702bd91371830501c1c5
marrero171 <50031007+marrero171@users.noreply.github.com>
2023-03-03 19:53:38 -0400

    controllers access only frontend/backend respectively

--- Phase-1/frontend_model/profileModel.py
+++ Phase-1/frontend_model/profileModel.py
@@ -1,5 +1,8 @@
 # Simulated database of users
 from backend_model.accountsModel import *
-
+from backend_controller.accountsController import editaccountcontroller
 def getUserModel(customer):
     return getaccountmodel(customer, admin=False)
+
+def changeinfocontroller(acc, infolist):
+    editaccountcontroller(acc, edits=infolist, admin=False)
