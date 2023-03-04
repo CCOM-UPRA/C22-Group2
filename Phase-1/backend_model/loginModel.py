92eb5cad33f740f069fb05fc7930fa2062f0dbfd
marrero171 <50031007+marrero171@users.noreply.github.com>
2023-03-03 20:03:16 -0400

    Last feex for this I swear

--- Phase-1/backend_model/loginModel.py
+++ Phase-1/backend_model/loginModel.py
@@ -1,9 +1,9 @@
 from flask import session
-from backend_controller.accountsController import *
+from backend_model.accountsModel import *
 
 def loginmodel(email : str, password : str):
     # Receive email and password to check in the "database"
-    logins = getaccounts()
+    logins = getaccountsmodel(admin=True)
 
     for key, admin in dict(logins).items():
             if dict(admin).get('c_email') == email:
