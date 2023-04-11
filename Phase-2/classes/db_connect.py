b9ee350f84b7c2e7f79c234657915eab6505311e
marrero171 <50031007+marrero171@users.noreply.github.com>
2023-04-11 10:41:19 -0400

    tweaks to class

--- Phase-2/classes/db_connect.py
+++ Phase-2/classes/db_connect.py
@@ -1,8 +1,8 @@
 import pymysql
 # Use this to access database
-class DBConnect():
+class DBConnect(pymysql.connections.Connection):
     def __init__(self) -> None:
-        self.conn = pymysql.connections.Connection(host='sql9.freemysqlhosting.net',
+        super(DBConnect, self).__init__(host='sql9.freemysqlhosting.net',
                     db='sql9607915',
                     user='sql9607915',
                     password='Sz7jbHnAwK',
--- Phase-2/frontend_model/loginModel.py
+++ Phase-2/frontend_model/loginModel.py
@@ -7,6 +7,31 @@ def loginmodel(email : str, password : str):
     # Receive email and password to check in the "database"
 
     db = DBConnect()
+    user = []
+    cur = db.cursor()
+    cur.execute("SELECT * from customer WHERE c_email = %s", email)
+    userFound = cur.fetchall()
+    for users in userFound:
+        user.append({"id": users[0], "name": users[1], "last_name": users[2], "address_line1": users[3],
+                    "address_line2": users[4], "city": users[5], "state": users[6], "zipcode": users[7],
+                    "email": users[8], "password": users[9], "phone_number": users[10], "card_name": users[11],
+                    "card_type": users[12], "card_number": users[13], "exp_date": users[14], "status": users[15]})
+
+    # Save user info in list
+
+    # sha256_crypt.encrypt("password") = this is what is used to encrypt a password
+    # sha256_crypt.verify(password_unhashed, password_hashed) = this is what is used to compare an unhashed and hashed password
+
+    for u in user:
+        print("Hashed password from user: ", u['password'])
+        if email == u['email'] and sha256_crypt.verify(password, u['password']) is True:
+            session['customer'] = u['id']
+            # Create the session['customer'] saving the customer ID if user is found
+            return "true"
+        else:
+            # If it didn't find user
+            return "false"
+    
     logins = getaccountsmodel(admin=False)
 
     for key, user in dict(logins).items():
