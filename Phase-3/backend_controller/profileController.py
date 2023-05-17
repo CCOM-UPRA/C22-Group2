fdd95cf328cd2aae9f22020fdeb639a9dce24a22
eddfig <eddy.figueroa1@upr.edu>
2023-05-17 19:00:55 -0400

    edit profile functionality

--- Phase-3/backend_controller/profileController.py
+++ Phase-3/backend_controller/profileController.py
@@ -3,3 +3,15 @@ from backend_model.profileModel import *
 def getUser(administrator):
     user = getUserModel(administrator)
     return user
+
+
+# -------------------- profile info--------------------
+
+def edit_profile(fname, lname, email):
+    editprofilemodel(fname, lname, email)
+
+
+# --------------------Contact number----------------------
+
+def edit_number(pnumber):
+    editnumbermodel(pnumber)
