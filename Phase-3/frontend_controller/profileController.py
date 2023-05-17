0e0996f20fb9ccc59b30325d0991c5380f770443
zulymargs <zulymargs@gmail.com>
2023-05-17 16:27:06 -0400

    Payment Methods works!!

--- Phase-3/frontend_controller/profileController.py
+++ Phase-3/frontend_controller/profileController.py
@@ -12,9 +12,12 @@ def getUser(customer):
 def edit_profile(fname, lname, email):
     editprofilemodel(fname, lname, email)
 
-#payment info
-def edit_payment(name, c_type, number, exp_date):
-    editpaymentmodel(name, c_type, number, exp_date)
+#-------------------payment info-----------------------------
+def edit_payment(card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode, payment_id):
+    editpaymentmodel(card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode, payment_id)
+
+def add_payment(card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode):
+    addpaymentmodel(card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode)
 
 def getPayment(customer):
     return getPaymentModel(customer)
@@ -29,8 +32,8 @@ def edit_address(aline1, aline2, state, zipcode, city):
 
 
 
-def edit_billaddress(aline1, aline2, state, zipcode, city):
-    edit_billaddressmodel(aline1, aline2, state, zipcode, city)
+# def edit_billaddress(aline1, aline2, state, zipcode, city):
+#     edit_billaddressmodel(aline1, aline2, state, zipcode, city)
     
 def edit_number(pnumber):
     editnumbermodel(pnumber)
--- Phase-3/frontend_model/profileModel.py
+++ Phase-3/frontend_model/profileModel.py
@@ -1,10 +1,10 @@
-# Simulated database of users
+#  database of users
 from classes.db_connect import DBConnect
 from flask import session
 
+#User in session
 def getUserModel(customer):
     db = DBConnect()
-    #sql = "SELECT * FROM customer NATURAL JOIN payment_method NATURAL JOIN shipping_address WHERE customer_id = %s"
     sql = "SELECT * FROM customer WHERE customer_id = %s"
     result = db.query(sql, (customer)).pop()
     return result
@@ -23,18 +23,6 @@ def editprofilemodel(fname, lname, email):
         print(f"Error occurred: {e}")
         return None
 
-#Change contact number
-def editnumbermodel(pnumber):
-    db = DBConnect()
-    try:
-        db.execute("UPDATE customer SET first_name = %s, last_name = %s, "
-                    "email = %s WHERE customer_id = %s",
-                    (fname, lname, email, session['customer']))
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        print(f"Error occurred: {e}")
-        return None
 
 #Change contact number
 def editnumbermodel(pnumber):
@@ -49,7 +37,7 @@ def editnumbermodel(pnumber):
         print(f"Error occurred: {e}")
         return None
     
-
+#S
 def getAddressModel(customer):
     db = DBConnect()
     sql = "SELECT * FROM shipping_address WHERE customer_id = %s"
@@ -71,16 +59,34 @@ def getPaymentModel(customer):
     print(result)
     return result
 
-def edit_billaddressmodel(aline1, aline2, state, zipcode, city):
-    db = DBConnect()
-    db.execute("UPDATE payment_method SET bill_address_line1 = %s, bill_address_line2 = %s, "
-                "bill_city = %s, bill_state = %s, bill_zipcode = %s WHERE customer_id = %s",
-                (aline1, aline2, city, state, zipcode, session['customer']))
+# def edit_billaddressmodel(aline1, aline2, state, zipcode, city):
+#     db = DBConnect()
+#     db.execute("UPDATE payment_method SET bill_address_line1 = %s, bill_address_line2 = %s, "
+#                 "bill_city = %s, bill_state = %s, bill_zipcode = %s WHERE customer_id = %s",
+#                 (aline1, aline2, city, state, zipcode, session['customer']))
 
-def editpaymentmodel(name, c_type, number, exp_date):
+def editpaymentmodel(card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode, payment_id):
     db = DBConnect()
-    db.execute("UPDATE payment_method SET card_name = %s, card_number = %s, "
-                "card_type = %s, card_exp_date = %s WHERE customer_id = %s",
-                (name, number, c_type, exp_date, session['customer']))
+    try:
+        db.execute("UPDATE payment_method SET card_name = %s, card_type = %s, "
+                "card_exp_date = %s, card_number = %s, bill_address_line1 = %s, bill_address_line2 = %s, "
+                "bill_city = %s, bill_state = %s, bill_zipcode = %s WHERE customer_id = %s AND payment_id = %s",
+                (card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode, session['customer'],payment_id))
+        db.commit()
 
+    except Exception as e:
+        db.rollback()
+        print(f"Error occurred: {e}")
+        return None
+
+def addpaymentmodel(card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode):
+    db = DBConnect()
+    try:
+        db.execute("INSERT INTO payment_method (card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode, customer_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(card_name, card_type, card_exp_date, card_number, bill_address_line1 ,  bill_address_line2, bill_city, bill_state, bill_zipcode, session['customer']))
+        db.commit()
+
+    except Exception as e:
+        db.rollback()
+        print(f"Error occurred: {e}")
+        return None
 
